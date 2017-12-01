f6b8fcbbafb37aa13e3ad6d171b170e11545def8
Jannik Vogel <email@jannikvogel.de>
2017-12-01 04:40:53 +0100

    Support other file formats and show first image longer

--- svg-slideshow.py
+++ svg-slideshow.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import imghdr
+
 import base64
 import sys
 
@@ -9,8 +11,8 @@ frames = len(args)
 
 print("Trying to load %d images" % frames)
 
-width = 648
-height = 506
+width = 800
+height = 500
 fade = 500 #ms
 delay = 2000 #ms
 
@@ -24,9 +26,16 @@ f.write('<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http
 
 t = 0
 
-with open(args[-1], "rb") as image_file:
-  encoded = base64.b64encode(image_file.read())
-f.write('<image opacity="1.0" width="%d" height="%d" xlink:href="data:image/png;base64,%s">\n' % (width, height, encoded.decode("utf-8")))
+def startImage(path):
+  with open(path, "rb") as image_file:
+    buf = image_file.read()
+    fmt = imghdr.what(None, buf)
+    encoded = base64.b64encode(buf)
+  f.write('<image opacity="1.0" width="%d" height="%d" xlink:href="data:image/%s;base64,%s">\n' % (width, height, fmt, encoded.decode("utf-8")))
+
+args = args[1:] + [args[0]]
+
+startImage(args[-1])
 f.write('</image>\n')
 
 for p in args:
@@ -35,24 +44,21 @@ for p in args:
 #    <image opacity="1.0" width="640" height="480" xlink:href="2017-11-28-235151_648x506_scrot.png">
 #    </image>
 
-  with open(p, "rb") as image_file:
-    encoded = base64.b64encode(image_file.read())
+  print('Adding "%s"' % p)
+  #encoded = bytes()
 
-    print('Adding "%s"' % p)
-    #encoded = bytes()
+  startImage(p)
+  f.write('  <animate attributeName="opacity" attributeType="XML"')
+  f.write('                 repeatCount="indefinite"')
+  f.write('                 begin="0"')
+  f.write('                 dur="%dms"' % dur)
+  t += delay
+  f.write('                 keyTimes="0;%f;%f;1.0"' % (t / dur, (t + fade) / dur))
+  t += fade
+  f.write('                 values="0.0;0.0;1.0;1.0"')
+  f.write('                 calcMode="linear" />\n')
+  f.write('</image>\n')
 
-    f.write('<image opacity="0.0" width="%d" height="%d" xlink:href="data:image/png;base64,%s">\n' % (width, height, encoded.decode("utf-8")))
-    f.write('  <animate attributeName="opacity" attributeType="XML"')
-    f.write('                 repeatCount="indefinite"')
-    f.write('                 begin="0"')
-    f.write('                 dur="%dms"' % dur)
-    f.write('                 keyTimes="0;%f;%f;1.0"' % (t / dur, (t + fade) / dur))
-    t += fade
-    t += delay
-    f.write('                 values="0.0;0.0;1.0;1.0"')
-    f.write('                 calcMode="linear" />\n')
-    f.write('</image>\n')
-
-    print("Processed %d / %d" % (t, dur))
+  print("Processed %d / %d" % (t, dur))
 
 f.write('</svg>')
